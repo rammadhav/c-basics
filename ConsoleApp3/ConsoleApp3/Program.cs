8fccddc04da2b75e1e97d67fc1f426e6fb37acee
rammadhav <madhav9th@gmail.com>
2019-01-16 21:44:19 +0530

    c# sample programs

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 2;
+            if (number < 5)
+            {
+                Console.WriteLine("{0} is less than 5", number);
+            }
+
+            Console.WriteLine("This statement is always executed.");
+            Console.ReadLine();
+        }
+    }
+}
